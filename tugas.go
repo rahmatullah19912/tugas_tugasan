a203e7c2f9f717c494f825f1cf6c2d0192e5778f
rahmatullah <rahmatullah19912@gmail.com>
2020-11-07 16:45:16 +0800

    curhat

--- tugas.go
+++ tugas.go
@@ -5,5 +5,6 @@ import (
 )
 
 func main() {
-	fmt.Println("aku dan dilan")
+	fmt.Println("aku dan dilan cuman teman biasa")
+	fmt.Println("karena desktop handphone terdapat gambar wanita lain yang sangat mirip dengan dilan")
 }
